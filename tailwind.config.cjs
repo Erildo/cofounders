56c33b8bb4174ea25c32230aecb31592dd17773e
Erildo Shuli <erildoshuli@gmail.com>
2022-09-06 16:36:27 +0200

    darkside on

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+
   theme: {
     extend: {},
   },
